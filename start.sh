4e6cbb61404dfa546caf1089b7921c9354cf0db0
Ainsley Chong <Ainsley@factual.com>
2015-09-17 22:48:59 +0000

    Added logging

--- start.sh
+++ start.sh
@@ -12,4 +12,13 @@ curl -s $SINOPIA_CONFIG > /opt/sinopia/config.yaml
 echo "Received sinopia config:"
 cat /opt/sinopia/config.yaml
 
+echo "Current user:"
+whoami
+
+echo "Storage:"
+ls -lrt /opt/sinopia/storage
+
+echo "Auth:"
+ls -lrt /opt/sinopia/auth
+
 node /opt/sinopia/node_modules/sinopia/bin/sinopia --config /opt/sinopia/config.yaml
