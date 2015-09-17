7d381606e7e1564caa8b82c6b3f15f911b93e096
Ainsley Chong <Ainsley@factual.com>
2015-09-17 22:56:41 +0000

    Test

--- start.sh
+++ start.sh
@@ -15,10 +15,21 @@ cat /opt/sinopia/config.yaml
 echo "Current user:"
 whoami
 
+echo "Base directory:"
+ls -lrt /opt/sinopia
+
 echo "Storage:"
 ls -lrt /opt/sinopia/storage
 
+echo "Write to storage:"
+touch /opt/sinopia/storage/test
+
 echo "Auth:"
 ls -lrt /opt/sinopia/auth
+
+echo "Write to Auth:"
+touch /opt/sinopia/auth/test
+
+echo "Write to storage:"
 
 node /opt/sinopia/node_modules/sinopia/bin/sinopia --config /opt/sinopia/config.yaml
